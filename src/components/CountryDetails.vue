b172f9df83861b108730258df91da221d25f2524
mohammedqasim91 <mohamadqasim888@gmail.com>
2023-08-19 11:15:10 +0200

    done

---
+++ src/components/CountryDetails.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+const route = useRoute();
+const country = ref({});
+const fetchCountry = async () => {
+    const id = route.params.alpha3Code;
+    const response = await fetch(
+        `https://ih-countries-api.herokuapp.com/countries/${id}`
+    );
+    const data = await response.json();
+    country.value = data;
+};
+onMounted(() => {
+    fetchCountry();
+});
+</script>
+
+<template>
+    <p>Country View{{ country }}{{ route.params }}</p>
+</template>
+
+<style scoped>
+p {
+    margin: 30px 50px;
+}
+</style>
